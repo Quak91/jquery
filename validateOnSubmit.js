682f34a7587d0f519b2b441cd2b616f0c7f4e261
Quak91 <piotr.quak@gmail.com>
2015-05-26 16:54:00 +0200

    nowe pliki js z podstawowymi założeniami

---
+++ validateOnSubmit.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+
+	$("#form").submit(function() {
+		/*
+			tutaj będzie walidacja formularza, zależnie od jej wyniku formularz zostanie wysłany(lub nie)
+		*/
+	})
+
+});
---
+++ validation.js
@@ -0,0 +1,10 @@
+(function ($) {
+
+	/*
+		tutaj będą funkcje do walidacji:
+		- na podstawie wyrażenia regularnego
+		- pola email
+		- na podstawie złożoności hasła
+	*/
+
+})(jQuery);
