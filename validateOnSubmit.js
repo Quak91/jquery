51492dc0e84ed0a6aa9724f1a7b0740545bda2cd
Quak91 <piotr.quak@gmail.com>
2015-05-29 02:41:16 +0200

    walidacja na podstawie złożoności hasła

--- validateOnSubmit.js
+++ validateOnSubmit.js
@@ -1,17 +1,24 @@
 $(document).ready(function (){
 
 	$("#form").submit(function() {
-
 		var isValid = true;
-
 		$("#mail").validateEmail();
 		$("#firstname").validatePattern({pattern: /^Jan|Piotr|Alicja|Katarzyna|Michał$/i });
 		$("#lastname").validatePattern({pattern: /^[A-ZĘÓĄŚŁŻŹĆŃ][a-zęóąśłżźćń]{2,}$/ });
+		$("#password").validatePassword();
 		
-		if($("#mail, #firstname, #lastname").hasClass("wrong")) { isValid = false; }
+		if($("#mail, #firstname, #lastname, #password").hasClass("wrong")) { isValid = false; }
 
 		if(isValid === false) { return false; }
+	});
 
+	$("#password").keyup(function() {
+		$("#password").validatePassword();
+		if($("#password").hasClass("wrong")) {
+			$("#pwd_strength").text("Słabe hasło");
+		} else {
+			$("#pwd_strength").text("Dobre hasło");
+		}
 	});
 
 });
--- validation.js
+++ validation.js
@@ -27,4 +27,62 @@
 		});
 	};
 
+	$.fn.validatePassword = function() {
+		return this.each(function() {
+			var password = $(this).val().toLowerCase(); // dla ułatwienia hasło nie zawiera wielkich liter
+			var points = 0;
+
+			var numberOfCharacters = 0;
+			var lettersAndDigits = 0;
+			var symbols = 0;
+			var lettersOnly = 0;
+			var digitsOnly = 0;
+			var symbolsOnly = 0;
+			var repeatCharacters = 0;
+
+			// za każdy znak dodaję 5pkt
+			numberOfCharacters = password.length * 5;
+
+			// jeżeli hasło zawiera conajmniej 1 literę i 1 cyfrę to dodaję 1 pkt za każdy znak
+			if((/[0-9]/).test(password) && (/[a-z]/).test(password)) {
+				lettersAndDigits = password.length;
+			}
+
+			// jeżeli hasło składa się z samych liter/cyfr/znaków specjalnych to odejmuję 1pkt za każdy znak
+			if((/^[0-9]+$/).test(password)) digitsOnly = password.length;
+			if((/^[a-z]+$/).test(password)) lettersOnly = password.length;
+			if((/^[\W]+$/).test(password)) symbolsOnly = password.length;
+
+			// jeżeli hasło nie składa się z samych znaków specjalnych to za każdy znak specjalny dodaję 2pkt
+			if((/\W/).test(password) && symbolsOnly === 0) {
+				symbols = password.match(/\W/g).length * 2;
+			}
+
+			// odejmuję 2pkt za każdy powtarzający się znak
+			var differentSymbols = [];
+			var isDiff;
+			for(var i=0; i<password.length; i++) {
+				isDiff = true;
+				for(var j=0; j<=differentSymbols.length; j++) {
+					if(password.charAt(i) == differentSymbols[j])
+						isDiff = false;
+				}
+				if(isDiff === true)
+					differentSymbols.push(password.charAt(i));
+			}
+			repeatCharacters = (password.length - differentSymbols.length) * 2;
+
+			points = numberOfCharacters + lettersAndDigits + symbols - repeatCharacters - lettersOnly - digitsOnly - symbolsOnly;
+			console.log(points);
+
+			if(points > 50) {
+				$(this).removeClass("wrong");
+				$(this).addClass("ok");
+			} else {
+				$(this).removeClass("ok");
+				$(this).addClass("wrong");
+			}
+		});
+	};
+
 })(jQuery);
